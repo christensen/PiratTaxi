7d29d8f6f6144922f1bddf36aeedf2cef7827736
Jens Christensen <jens.christensen82@gmail.com>
2017-02-25 12:22:12 +0100

    Initial Commit

---
+++ PiratTaxiTests/PiratTaxiTests.swift
@@ -0,0 +1,36 @@
+//
+//  PiratTaxiTests.swift
+//  PiratTaxiTests
+//
+//  Created by Jens Christensen on 2017-02-25.
+//  Copyright © 2017 Christensen Software. All rights reserved.
+//
+
+import XCTest
+@testable import PiratTaxi
+
+class PiratTaxiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
